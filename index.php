57e5bb3dbe7d8075673d46d929d92ac7c41e2912
unknown <bobby.gerez@yahoo.com>
2020-03-20 11:37:13 +0800

    Updates

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+ 
+require 'vendor/autoload.php';
+ 
+echo lego();
+ 
+use Acme\Db;
+use Acme\User;
+use Acme\Page;
+ 
+$page1 = new Page();
+
+$c = new Cmautoload();
+echo $c->classmap();
---
+++ src/page.php
@@ -0,0 +1,9 @@
+<?php
+namespace Acme;
+
+class Page {
+    public function __construct()
+    {
+        echo "hello, i am a page.";
+    }
+}
