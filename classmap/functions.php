ff27be7557bfbf9b5006576b119d76feaee5aa63
bobby gerez <bobby.gerez@monstar-lab.com>
2025-09-06 11:09:26 +0800

    updates

--- classmap/functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-Class Cmautoload {
- 
-	  public function classmap () {
-		  return 'that knows how to autoload with a classmap! ';
-	  }
-}
--- src/page.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Acme;
-
-class Page {
-    public function __construct()
-    {
-        echo "hello, i am a page.";
-    }
-}
